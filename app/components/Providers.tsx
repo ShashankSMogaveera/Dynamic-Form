d450058229a63ab9f3898488d7e956bf5e8e89e4
Arigela Pavani <pavaniarigela777@gmail.com>
2025-03-21 15:55:46 +0530

    changes made

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,6 +1,6 @@
 "use client"; 
 import { Provider } from "react-redux";
-import store from "../store/store";
+import store from "@/app/store/store";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
